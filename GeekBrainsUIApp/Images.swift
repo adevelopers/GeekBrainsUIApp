8df498c38bf6d36c9e9d898f71615ed94d17823f
Kirill Khudyakov <hudiakov@gmail.com>
2019-11-23 01:52:09 +0300

    Добавил иконки VK-Logo

---
+++ GeekBrainsUIApp/Images.swift
@@ -0,0 +1,15 @@
+//
+//  Images.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 23.11.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIImage {
+    static let logo = UIImage(imageLiteralResourceName: "VK-Logo")
+    static let logoDarkTheme = UIImage(imageLiteralResourceName: "VK-Logo-inverted")
+}
