90f1e7aac46912441080ab5dd6cf01aa17fff719
Kirill Khudyakov <hudiakov@gmail.com>
2019-11-23 00:23:10 +0300

    Initial Commit

---
+++ GeekBrainsUIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 23.11.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
