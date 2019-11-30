beb73e3e648fde03f4a892e53ec0ab9846257692
Kirill Khudyakov <hudiakov@gmail.com>
2019-11-30 22:53:03 +0300

    Добавил модель пользователя и пол

---
+++ GeekBrainsUIApp/Models/Sex.swift
@@ -0,0 +1,14 @@
+//
+//  Sex.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 30.11.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+enum Sex {
+    case unsetted, male, female
+}
---
+++ GeekBrainsUIApp/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 30.11.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+struct User {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phone: String
+    let sex: Sex
+}
