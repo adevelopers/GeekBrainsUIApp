946950d9940f5c4fd8efd68fe645a6585f69052d
Kirill Khudyakov <hudiakov@gmail.com>
2019-11-30 23:21:16 +0300

    Добавил модель Сообщество

---
+++ GeekBrainsUIApp/Models/Group.swift
@@ -0,0 +1,17 @@
+//
+//  Group.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 30.11.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Group {
+    let id: String
+    let name: String
+    let description: String
+    let avatar: String
+}
