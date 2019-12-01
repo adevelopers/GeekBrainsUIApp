069e7a2198705f50e1f7b1d96014a211ee91b16a
Kirill Khudyakov <hudiakov@gmail.com>
2019-12-01 14:24:16 +0300

    Добавил FriendCell + FriendListViewController

---
+++ GeekBrainsUIApp/UI/FriendListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FriendListViewController.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 01.12.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UITableViewController {
+
+    var items = User.items
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    // MARK: DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FriendCell.reuseId, for: indexPath) as? FriendCell else {
+            return UITableViewCell()
+        }
+        let model = items[indexPath.row]
+        cell.nameLabel.text = model.name
+        cell.locationLabel.text = model.location
+        cell.setAvatar(with: model.avatar)
+        return cell
+    }
+
+}
+
