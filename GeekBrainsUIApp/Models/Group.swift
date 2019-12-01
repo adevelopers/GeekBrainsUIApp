899101c111dddd5e73ffb238deb80efe0eb7c423
Kirill Khudyakov <hudiakov@gmail.com>
2019-12-01 14:26:31 +0300

    Добавил демо данные для User + Group

--- GeekBrainsUIApp/Models/Group.swift
+++ GeekBrainsUIApp/Models/Group.swift
@@ -15,3 +15,16 @@ struct Group {
     let description: String
     let avatar: String
 }
+
+
+extension Group {
+    static let items = [
+        Group(id: "1", name: "Интернет вещей", description: "IoT, Smart devices", avatar: "iot"),
+        Group(id: "2", name: "Соус", description: "Рецепты", avatar: "sous"),
+        Group(id: "3", name: "Идеи для дома", description: "другие хобби", avatar: "idea"),
+        Group(id: "4", name: "Фитнесс", description: "", avatar: "fitness"),
+        Group(id: "5", name: "Мобильная разработка", description: "", avatar: "ios2"),
+        Group(id: "6", name: "PeerLab", description: "", avatar: "ios"),
+        Group(id: "7", name: "Еда", description: "Стейки", avatar: "meat"),
+    ]
+}
--- GeekBrainsUIApp/Models/User.swift
+++ GeekBrainsUIApp/Models/User.swift
@@ -27,4 +27,26 @@ extension User {
     }
 }
 
+
+extension User {
+    static let items = [
+        User(id: "1", firstName: "Дмитрий", lastName: "Глуховский", email: "dima@mail.ru", phone: "+7 903 000-00-00", avatar: "dima", sex: .male, location: "Москва"),
+        User(id: "2", firstName: "Сандрин", lastName: "Лимс", email: "sandrin.lims@mail.ru", phone: "+7 903 000-00-00", avatar: "lena", sex: .female, location: "Москва"),
+        User(id: "3", firstName: "Эон", lastName: "Флакс", email: "eon.flux@mail.ru", phone: "+7 903 000-00-00", avatar: "jana", sex: .female, location: nil),
+        User(id: "4", firstName: "Игорь", lastName: "Ботов", email: "botov@mail.ru", phone: "+7 917 000-00-00", avatar: "igor", sex: .male, location: "Санкт-Петербург"),
+        User(id: "5", firstName: "Мария", lastName: "Нарусова", email: "narusova@mail.ru", phone: "+7 903 000-00-00", avatar: "mary", sex: .female, location: "Екатеринбург"),
+        User(id: "6", firstName: "Захар", lastName: "", email: "zevs@mail.ru", phone: "+7 903 000-00-00", avatar: "zahar", sex: .female, location: "Москва"),
+        User(id: "1", firstName: "Дмитрий", lastName: "Глуховский", email: "dima@mail.ru", phone: "+7 903 000-00-00", avatar: "dima", sex: .male, location: "Москва"),
+        User(id: "2", firstName: "Сандрин", lastName: "Лимс", email: "sandrin.lims@mail.ru", phone: "+7 903 000-00-00", avatar: "lena", sex: .female, location: "Москва"),
+        User(id: "3", firstName: "Эон", lastName: "Флакс", email: "eon.flux@mail.ru", phone: "+7 903 000-00-00", avatar: "jana", sex: .female, location: nil),
+        User(id: "4", firstName: "Игорь", lastName: "Ботов", email: "botov@mail.ru", phone: "+7 917 000-00-00", avatar: "igor", sex: .male, location: "Санкт-Петербург"),
+        User(id: "1", firstName: "Дмитрий", lastName: "Глуховский", email: "dima@mail.ru", phone: "+7 903 000-00-00", avatar: "dima", sex: .male, location: "Москва"),
+        User(id: "2", firstName: "Сандрин", lastName: "Лимс", email: "sandrin.lims@mail.ru", phone: "+7 903 000-00-00", avatar: "lena", sex: .female, location: "Москва"),
+        User(id: "3", firstName: "Эон", lastName: "Флакс", email: "eon.flux@mail.ru", phone: "+7 903 000-00-00", avatar: "jana", sex: .female, location: nil),
+        User(id: "4", firstName: "Игорь", lastName: "Ботов", email: "botov@mail.ru", phone: "+7 917 000-00-00", avatar: "igor", sex: .male, location: "Санкт-Петербург"),
+        User(id: "1", firstName: "Дмитрий", lastName: "Глуховский", email: "dima@mail.ru", phone: "+7 903 000-00-00", avatar: "dima", sex: .male, location: "Москва"),
+        User(id: "2", firstName: "Сандрин", lastName: "Лимс", email: "sandrin.lims@mail.ru", phone: "+7 903 000-00-00", avatar: "lena", sex: .female, location: "Москва"),
+        User(id: "3", firstName: "Эон", lastName: "Флакс", email: "eon.flux@mail.ru", phone: "+7 903 000-00-00", avatar: "jana", sex: .female, location: nil),
+        User(id: "4", firstName: "Игорь", lastName: "Ботов", email: "botov@mail.ru", phone: "+7 917 000-00-00", avatar: "igor", sex: .male, location: "Санкт-Петербург"),
+    ]
 }
