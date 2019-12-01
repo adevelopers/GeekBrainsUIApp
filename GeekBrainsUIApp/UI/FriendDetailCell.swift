0044bd341b7f1686d1c05a159148b9ff05d61eb6
Kirill Khudyakov <hudiakov@gmail.com>
2019-12-01 15:58:54 +0300

    Добавил FriendDetailCell

---
+++ GeekBrainsUIApp/UI/FriendDetailCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendDetailCell.swift
+//  GeekBrainsUIApp
+//
+//  Created by Kirill Khudiakov on 01.12.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailCell: UICollectionViewCell {
+    
+}
