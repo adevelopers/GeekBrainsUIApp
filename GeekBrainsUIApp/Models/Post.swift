de194c992fd623b6dc1a787cf65ddf25d55dcefe
Kirill <hudiakov@gmail.com>
2019-12-07 11:41:55 +0300

    Добавил модель Post

---
+++ GeekBrainsUIApp/Models/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  GeekBrainsUIApp
+//
+//  Created by Кирилл Худяков on 07.12.2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Post {
+    let id: String
+    let title: String
+    let text: String
+    let publishedDate: String
+}
