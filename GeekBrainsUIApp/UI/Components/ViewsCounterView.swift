f90882c03ddea518b8ff5d29fefb4a6fb007d39d
Kirill Khudyakov <hudiakov@gmail.com>
2019-12-15 12:08:20 +0300

    Добавил констрейнты в ViewsCounterView

--- GeekBrainsUIApp/UI/Components/ViewsCounterView.swift
+++ GeekBrainsUIApp/UI/Components/ViewsCounterView.swift
@@ -18,15 +18,45 @@ final class ViewsCounterView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setup()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        viewsCountLabel.textColor = .lightGray
+        viewsCountLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        viewsCountLabel.text = String(132)
+        
+        addSubviews([
+            viewsCountLabel,
+            viewsIcon
+        ])
+        
+        setupConstraints()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundColor = .green
+    }
+    
+    private func setupConstraints() {
+        viewsIcon.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewsIcon.leftAnchor.constraint(equalTo: leftAnchor),
+            viewsIcon.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
         
+        viewsCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewsCountLabel.leftAnchor.constraint(equalTo: viewsIcon.rightAnchor, constant: 8),
+            viewsCountLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            viewsCountLabel.widthAnchor.constraint(equalToConstant: 30),
+            viewsCountLabel.heightAnchor.constraint(equalToConstant: 25)
+        ])
     }
 }
